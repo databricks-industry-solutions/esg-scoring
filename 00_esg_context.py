34d4eb76e7a776dd1c64fc60666e546bb4dbc7e4
dbbnicole <dbbnicole>
2022-10-19 17:02:20 +0000

    Commit web-version notebooks

--- 00_esg_context.py
+++ 00_esg_context.py
@@ -1,30 +1,30 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC <img src=https://d1r5llqwmkrl74.cloudfront.net/notebooks/fsi/fs-lakehouse-logo-transparent.png width="600px">
+# MAGIC <img src=https://d1r5llqwmkrl74.cloudfront.net/notebooks/fs-lakehouse-logo.png width="600px">
 # MAGIC 
-# MAGIC [![DBR](https://img.shields.io/badge/DBR-10.4ML-red?logo=databricks&style=for-the-badge)](https://docs.databricks.com/release-notes/runtime/10.4ml.html)
-# MAGIC [![CLOUD](https://img.shields.io/badge/CLOUD-ALL-blue?logo=googlecloud&style=for-the-badge)](https://databricks.com/try-databricks)
-# MAGIC [![POC](https://img.shields.io/badge/POC-10 days-green?style=for-the-badge)](https://databricks.com/try-databricks)
+# MAGIC [![COMPLEXITY](https://img.shields.io/badge/COMPLEXITY-201-orange)]()
+# MAGIC [![POC](https://img.shields.io/badge/POC-5d-red)]()
 # MAGIC 
-# MAGIC *The future of finance goes hand in hand with social responsibility, environmental stewardship and corporate ethics. 
-# MAGIC In order to stay competitive, Financial Services Institutions (FSI)  are increasingly  disclosing more information 
-# MAGIC about their environmental, social and governance (ESG) performance. By better understanding and quantifying the 
-# MAGIC sustainability and societal impact of any investment in a company or business, FSIs can mitigate reputation risk and 
-# MAGIC maintain the trust with both their clients and shareholders. At Databricks, we increasingly hear from our customers 
-# MAGIC that ESG has become a C-suite priority. This is not solely driven by altruism but also by economics: 
-# MAGIC Higher ESG ratings are generally positively correlated with valuation and profitability while negatively correlated with 
-# MAGIC volatility ([source](https://corpgov.law.harvard.edu/2020/01/14/esg-matters/)). 
-# MAGIC In this solution, we offer a novel approach to sustainable finance by combining NLP techniques and news analytics to 
-# MAGIC extract key strategic ESG initiatives and learn companies' commitments to corporate responsibility.*
+# MAGIC 
+# MAGIC *The future of finance goes hand in hand with social responsibility, environmental stewardship and corporate ethics. In order to stay competitive, Financial Services Institutions (FSI)  are increasingly  disclosing more information about their environmental, social and governance (ESG) performance. By better understanding and quantifying the sustainability and societal impact of any investment in a company or business, FSIs can mitigate reputation risk and maintain the trust with both their clients and shareholders. At Databricks, we increasingly hear from our customers that ESG has become a C-suite priority. This is not solely driven by altruism but also by economics: [Higher ESG ratings are generally positively correlated with valuation and profitability while negatively correlated with volatility](https://corpgov.law.harvard.edu/2020/01/14/esg-matters/). In this solution, we offer a novel approach to sustainable finance by combining NLP techniques and news analytics to extract key strategic ESG initiatives and learn companies' commitments to corporate responsibility.*
+# MAGIC 
 # MAGIC 
 # MAGIC ___
-# MAGIC 
 # MAGIC <antoine.amend@databricks.com>
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC <img src='https://raw.githubusercontent.com/databricks-industry-solutions/esg-scoring/main/images/reference_architecture.png' width=800>
+# MAGIC <img src=https://d1r5llqwmkrl74.cloudfront.net/notebooks/fsi/esg_scoring/images/reference_architecture.png width="800px">
+
+# COMMAND ----------
+
+# MAGIC %run ./config/esg_config
+
+# COMMAND ----------
+
+# Initialize the environment
+tear_down()
 
 # COMMAND ----------
 
@@ -33,10 +33,10 @@
 # MAGIC 
 # MAGIC | library                                | description             | license    | source                                              |
 # MAGIC |----------------------------------------|-------------------------|------------|-----------------------------------------------------|
-# MAGIC | beautifulsoup4                         | Web scraper library     | MIT        | https://www.crummy.com/software/BeautifulSoup       |
 # MAGIC | PyPDF2                                 | PDF parser              | BSD        | https://pypi.org/project/PyPDF2                     |
-# MAGIC | NLTK                                   | NLP toolkit             | Apache2    | https://github.com/nltk/nltk                        |
-# MAGIC | Spacy                                  | NLP toolkit             | MIT        | https://spacy.io/                                   |
-# MAGIC | Wordcloud                              | Visualization library   | MIT        | https://github.com/amueller/word_cloud              |
-# MAGIC | PyYAML                                 | Reading Yaml files      | MIT        | https://github.com/yaml/pyyaml                      |
 # MAGIC | spark-gdelt                            | GDELT wrapper           | Apache2    | https://github.com/aamend/spark-gdelt               |
+# MAGIC | PyLDAvis                               | LDA visualizer          | MIT        | https://github.com/bmabey/pyLDAvis                  |
+# MAGIC | Gensim                                 | Topic modelling         | L-GPL2     | https://radimrehurek.com/gensim/                    |
+# MAGIC | Wordcloud                              | Visualization library   | MIT        | https://github.com/amueller/word_cloud              |
+# MAGIC | fuzzywuzzy                             | Fuzzy matching library  | GNU        | https://github.com/seatgeek/fuzzywuzzy              |
+# MAGIC | python-Levenshtein                     | Fuzzy matching library  | GNU        | https://github.com/ztane/python-Levenshtein         |
